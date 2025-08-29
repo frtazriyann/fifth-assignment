f4ddb27ccbb0fccdd4de7e65cd5e068fb3ade48a
tazriyan <fazlerabbitazriyan@gmail.com>
2025-08-29 23:10:19 +0600

    copy func

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ heartIcon('heart-brac')
 heartIcon('heart-rail')
 
 
+// Copy Functionality
+let copyCount = parseInt(document.getElementById('copyCount').innerText)
+function copyIcon(id1, id2) {
+    document.getElementById(id1).addEventListener('click', function () {
+        copyCount++
+        document.getElementById('copyCount').innerText = copyCount
+
+        let msg = document.getElementById(id2).innerText
+        console.log(msg);
+
+        navigator.clipboard.writeText(msg).then(function () {
+            alert(Number has copied: ${msg})
+        })
+    })
+}
+copyIcon('copy-national', 'msg-national')
+copyIcon('copy-police', 'msg-police')
+copyIcon('copy-fire', 'msg-fire')
+copyIcon('copy-ambulance', 'msg-ambulance')
+copyIcon('copy-child', 'msg-child')
+copyIcon('copy-corruption', 'msg-corruption')
+copyIcon('copy-electricity', 'msg-electricity')
+copyIcon('copy-brac', 'msg-brac')
+copyIcon('copy-rail', 'msg-rail')
+
+
